36c979b28741ea1d11cc0792e3a721cdfedb8cba
Omm18 <114692477+Omm18@users.noreply.github.com>
2024-03-18 23:40:58 +0530

    Update databse.js

--- Backend /databse.js
+++ Backend /databse.js
@@ -1,1 +1,13 @@
 
+const mongoose = require("mongoose");
+
+const mongoconnect = ()=>{
+    mongoose.connect("mongodb://localhost:27017/notebook").then(()=>{
+        console.log("database connected");
+        
+    }).catch((Error)=>{
+        console.log(Error);
+    })
+}
+
+module.exports = mongoconnect;
