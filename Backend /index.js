418014cc41ef52ae134246a55f95521ec6a99a3f
Omm18 <114692477+Omm18@users.noreply.github.com>
2024-03-18 23:41:28 +0530

    Update index.js

--- Backend /index.js
+++ Backend /index.js
@@ -1,1 +1,22 @@
 
+const mongoconnect = require("./databse");
+const express = require("express");
+const app = express();
+const authroutes = require("./routes/auth");
+const notesroute = require("./routes/Notes")
+var cors = require('cors')
+
+
+
+
+
+
+
+
+mongoconnect();
+app.use(cors())
+app.use(express.json())
+app.use("/get/auth",authroutes);
+app.use("/get/notes",notesroute)
+
+app.listen(5000);
